b9d161fcbbbdb204e643217a167818fc68abe0b3
Gabriel Stellato <gabrielstellato120@gmail.com>
2023-08-25 22:38:46 -0300

    Update cnpj.py
    Versão melhorada e reduzida da ferramenta de consulta de CNPJ.

--- cnpj.py
+++ cnpj.py
@@ -1,42 +1,46 @@
 import requests
 import json
 
-def buscar_dados():
+def tratar_cnpj(cnpj):
+    if cnpj == 'a':
+        print("CNPJ INVÁLIDO")
+    if cnpj == '1':
+        print("CNPJ INVÁLIDO")
+    if cnpj == '2':
+        print("CNPJ INVÁLIDO")
+    if cnpj == '9':
+        print("CNPJ INVÁLIDO")
+    else:
+        validar_cnpj(cnpj)
+
+def validar_cnpj(cnpj):
+    request = requests.get(f'https://api.nfse.io/validate/LegalEntities/taxNumber/{cnpj}')
+    validador = json.loads(request.content)
+    if not validador['valid']:
+        print("CNPJ INCORRETO")
+    else:
+        buscar_dados(cnpj)
+
+def buscar_dados(cnpj):
     request = requests.get(f'https://www.receitaws.com.br/v1/cnpj/{cnpj}')
     dados = json.loads(request.content)
-    print("Nome:")
-    print(dados['nome'])
-    print("Data de abertura:")
-    print(dados['data_situacao'])
-    print("Endereço:")
-    print(dados['logradouro'])
-    print(dados['numero'])
+    print("=============================")
+    print("Nome:", dados['nome'])
+    print("Data de abertura:", dados['data_situacao'])
+    print("Endereço:", dados['logradouro'], dados['numero'])
     print(dados['bairro'])
-    print(dados['cep'])
-    print(dados['uf'])
-    print("Contatos:")
-    print(dados['email'])
-    print(dados['telefone'])
-    print("Complementos:")
-    print(dados['natureza_juridica'])
-    print(dados['situacao'])
-
-
-
-def chr_remove(old, to_remove):
-    new_string = old
-    for x in to_remove:
-        new_string = new_string.replace(x, '')
-    return new_string
+    print(dados['cep'], dados['uf'])
+    print("Contatos:", dados['email'], dados['telefone'])
+    print("Complementos:", dados['natureza_juridica'])
+    print("Situação:", dados['situacao'])
 
 def exibir():
-    print("codado por c0ala.py ")
+    print("codado por c0ala.py")
 
-cnpj = input("Digite o cnpj: ")
-
+def main():
+    cnpj = input("Digite o CNPJ: ")
+    tratar_cnpj(cnpj)
+    exibir()
 
 if __name__ == '__main__':
- buscar_dados()
- exibir()
-
-
+    main()
